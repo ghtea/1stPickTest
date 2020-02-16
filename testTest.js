d5ad76606fc680d1343d9f1f021342f73fb6525e
PJH <eiirwp@gmail.com>
2020-02-16 12:32:17 +0900

    q

---
+++ testTest.js
@@ -0,0 +1,17 @@
+const = "Towers of Doom"
+ 
+  let colWinRate = currentMap + ' win_rate'
+  let colPlayRate = currentMap + ' popularity'
+  let colBanRate = currentMap + ' ban_rate'
+  
+  
+for (const [key, value] of Object.entries(data)) {
+  value['Point'] =
+    (100 - 77) * (data[colWinRate]/ 50 / 3.6) +
+      77 *
+        ((((data[colBanRate] + data[50])/ 100) * 88) /
+          16 /
+          19);
+}
+
+console.log(data)
