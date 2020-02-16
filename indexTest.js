608cef150b7af43f0962034cadc7ff03d1c7f0fc
PJH <eiirwp@gmail.com>
2020-02-16 16:04:35 +0900

    q

--- indexTest.js
+++ indexTest.js
@@ -64,9 +64,134 @@ function updateJson() {
   dataList = Object.values(data)
 }
 
+function makeRows() {
+  /*
+  dataListSorted = dataList.sort(compaireFunc("Point"));
+  console.log(dataListSorted)
+ 
+  if (rows.length > 0) {
+    for (var i = 0; i < numHero; i++) {
+      tbl.deleteRow(1);
+    }
+  }
+  */
+  for (var i = 0; i < numHero; i++) {
+    var row = tbl.insertRow(i + 1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
 
+    row.classList.add("rowTableMain");
+    /*row.classList.add("rowShow");*/
+    row.setAttribute("id", "rowHeroID" + dataList[i]["HeroID"]);
+    row.classList.add("rowDifficulty" + dataList[i]["Difficulty"]);
+    row.classList.add("rowRole" + dataList[i]["Role"]);
+    row.setAttribute("data-point", dataList[i]["Point"].toString());
+
+    cell1.innerHTML =
+      "<a target='_blank' rel='noopener noreferrer' href=" +
+      URLfront +
+      dataList[i]["URLID"] +
+      URLback +
+      "> <img src=" +
+      "heroImages/" +
+      dataList[i]["HeroID"] +
+      ".png" +
+      ">" +
+      "</a>";
+    
+    
+    switch (dataList[i]["Role"]) {
+      case "Tank":
+        roleInitial = "T";
+        break;
+      case "Bruiser":
+        roleInitial = "B";
+        break;
+      case "Melee":
+        roleInitial = "M";
+        break;
+      case "Ranged":
+        roleInitial = "R";
+        break;
+      case "Healer":
+        roleInitial = "H";
+        break;
+      case "Support":
+        roleInitial = "S";
+        break;
+    } 
+    
+    var divRoleTd = document.createElement("div");
+    var rectRole = document.createElement("div");
+    rectRole.setAttribute("class", "role" + dataList[i]["Role"]);
+    rectRole.innerHTML = roleInitial;
+    divRoleTd.appendChild(rectRole);
+    cell2.appendChild(divRoleTd);
+
+    for (var k = 0; k < parseInt(dataList[i]["Difficulty"]); k++) {
+      var rectDifficulty = [];
+      rectDifficulty[k] = document.createElement("div");
+      cell3.appendChild(rectDifficulty[k]);
+    }
+    cell3.setAttribute("class", "difficulty" + dataList[i]["Difficulty"]);
+
+    cell4.setAttribute("class", "cellMain");
+    var rectMain = document.createElement("div");
+    var rectMainWidth = (dataList[i][colWinRate] - 35) * numSizeWin;
+    var rectMainHeight =
+      (dataList[i][colPlayRate]+ dataList[i][colBanRate]) * numSizePlay;
+
+    rectMain.style =
+      "width:" +
+      rectMainWidth +
+      "px;height:" +
+      rectMainHeight +
+      "px; background: linear-gradient(200deg, rgba(105,245,168,1) 0%, rgba(17,226,97,1) 40%, rgba(17,226,97,1) 100%); ";
+    rectMain.setAttribute("class", "rectMain");
+    /* followings don't work
+        Rect.style.width = RectWidth + "px;" ;
+        Rect.style.height = RectHeight + "px;" ;
+        Rect.setAttribute("class", "boxWG");
+        */
+    cell4.appendChild(rectMain);
+
+    var divText = document.createElement("div");
+    var txtGames = (100 / dataList[i][colPlayRate]).toFixed(1);
+    var txtWinRate = dataList[i][colWinRate].toFixed(1);
+    divText.innerHTML = txtWinRate + "%" + "<br> 1 in " + txtGames + "G";
+    divText.setAttribute("class", "divRectText");
+    cell4.appendChild(divText);
+
+    /* var txtPoint = dataSorted[i]["Point"].toFixed(1); */
+
+    cell5.innerHTML = "<input type='checkbox' class = 'cbxPerHero'>";
+  }
+
+  cbxPerHeroList = document.getElementsByClassName("cbxPerHero");
+  for (var i = 0; i < numHero; i++) {
+    cbxPerHeroList[i].addEventListener("change", checkSome);
+  }
+}
   
 function  applyPointSortTable() {
+  let currentMap = document.getElementById("sltMap").value;
+  let currentDifficulty = document.getElementById("sltDifficulty").value;
+  let currentRatio = document.getElementById("rgRatio").value;
+  let rows = document.getElementsByClassName("rowTableMain");
+  
+  let colWinRate = currentMap + ' win_rate';
+  let colPlayRate = currentMap + ' popularity';
+  let colBanRate = currentMap + ' ban_rate';
+  for (var i = 0; i < numHero; i++) {
+    rows[i].get
+    dataList[i]
+  }
+
+
+
   let table, rows, switching, i, x, y, shouldSwitch;
   
   switching = true;
@@ -102,118 +227,7 @@ function  applyPointSortTable() {
 }
 
 
-function makeRows() {
-  /*
-  dataListSorted = dataList.sort(compaireFunc("Point"));
-  console.log(dataListSorted)
- 
-  if (rows.length > 0) {
-    for (var i = 0; i < numHero; i++) {
-      tbl.deleteRow(1);
-    }
-  }
-  */
 
-  for (var i = 0; i < numHero; i++) {
-    var row = tbl.insertRow(i + 1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-
-    row.classList.add("rowTableMain");
-    /*row.classList.add("rowShow");*/
-    row.setAttribute("id", "rowHeroID" + dataListSorted[i]["HeroID"]);
-    row.classList.add("rowDifficulty" + dataListSorted[i]["Difficulty"]);
-    row.classList.add("rowRole" + dataListSorted[i]["Role"]);
-    row.setAttribute("data-point", dataListSorted[i]["Point"].toString());
-
-    cell1.innerHTML =
-      "<a target='_blank' rel='noopener noreferrer' href=" +
-      URLfront +
-      dataListSorted[i]["URLID"] +
-      URLback +
-      "> <img src=" +
-      "heroImages/" +
-      dataListSorted[i]["HeroID"] +
-      ".png" +
-      ">" +
-      "</a>";
-    
-    
-    switch (dataListSorted[i]["Role"]) {
-      case "Tank":
-        roleInitial = "T";
-        break;
-      case "Bruiser":
-        roleInitial = "B";
-        break;
-      case "Melee":
-        roleInitial = "M";
-        break;
-      case "Ranged":
-        roleInitial = "R";
-        break;
-      case "Healer":
-        roleInitial = "H";
-        break;
-      case "Support":
-        roleInitial = "S";
-        break;
-    } 
-    
-    var divRoleTd = document.createElement("div");
-    var rectRole = document.createElement("div");
-    rectRole.setAttribute("class", "role" + dataListSorted[i]["Role"]);
-    rectRole.innerHTML = roleInitial;
-    divRoleTd.appendChild(rectRole);
-    cell2.appendChild(divRoleTd);
-
-    for (var k = 0; k < parseInt(dataListSorted[i]["Difficulty"]); k++) {
-      var rectDifficulty = [];
-      rectDifficulty[k] = document.createElement("div");
-      cell3.appendChild(rectDifficulty[k]);
-    }
-    cell3.setAttribute("class", "difficulty" + dataListSorted[i]["Difficulty"]);
-
-    cell4.setAttribute("class", "cellMain");
-    var rectMain = document.createElement("div");
-    var rectMainWidth = (dataListSorted[i][colWinRate] - 35) * numSizeWin;
-    var rectMainHeight =
-      (dataListSorted[i][colPlayRate]+ dataListSorted[i][colBanRate]) * numSizePlay;
-
-    rectMain.style =
-      "width:" +
-      rectMainWidth +
-      "px;height:" +
-      rectMainHeight +
-      "px; background: linear-gradient(200deg, rgba(105,245,168,1) 0%, rgba(17,226,97,1) 40%, rgba(17,226,97,1) 100%); ";
-    rectMain.setAttribute("class", "rectMain");
-    /* followings don't work
-        Rect.style.width = RectWidth + "px;" ;
-        Rect.style.height = RectHeight + "px;" ;
-        Rect.setAttribute("class", "boxWG");
-        */
-    cell4.appendChild(rectMain);
-
-    var divText = document.createElement("div");
-    var txtGames = (100 / dataListSorted[i][colPlayRate]).toFixed(1);
-    var txtWinRate = dataListSorted[i][colWinRate].toFixed(1);
-    divText.innerHTML = txtWinRate + "%" + "<br> 1 in " + txtGames + "G";
-    divText.setAttribute("class", "divRectText");
-    cell4.appendChild(divText);
-
-    /* var txtPoint = dataSorted[i]["Point"].toFixed(1); */
-
-    cell5.innerHTML = "<input type='checkbox' class = 'cbxPerHero'>";
-  }
-
-  cbxPerHeroList = document.getElementsByClassName("cbxPerHero");
-  for (var i = 0; i < numHero; i++) {
-    cbxPerHeroList[i].addEventListener("change", checkSome);
-  }
-}
 
 function hideSome() {
   let currentMap = document.getElementById("sltMap").value;
